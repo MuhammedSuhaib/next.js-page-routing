1157f6d3e94b9c6fabadc4a97303b7a131270f97
Suhaib Umair <154515857+MuhammedSuhaib@users.noreply.github.com>
2024-10-12 16:21:50 +0500

    added details and summary tags

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import style from "./home.module.css";
 import React from "react";
@@ -7,23 +6,32 @@ import Link from "next/link";
 function page() {
   return (
     <>
-    <h1 className={styles.h1}>This is Home Page</h1>
-    <br />
-    <nav className={style.Link} >
-      <div>
-        {" "}
-        <Link  href="/About">About</Link>
-      </div>
-      <div>
-        {" "}
-        <Link href="/Services">Services</Link>
-      </div>{" "}
-      <div>
-        {" "}
-        <Link href="/Contact">Contact</Link>
-      </div>
-    </nav>
-    
+      <h1 className={styles.h1}>This is Home Page</h1>
+      <br />
+      <nav className={style.Link}>
+        <div>
+          {" "}
+          <details>
+            <summary>
+              <Link href="/About">About</Link>
+            </summary>
+            <p>
+              <Link href="/About/Team">Team</Link>
+            </p>
+            <br />
+          </details>
+          <br />
+        </div>
+        <div>
+          {" "}
+          <Link href="/Services">Services</Link>
+        </div>{" "}
+        <br />
+        <div>
+          {" "}
+          <Link href="/Contact">Contact</Link>
+        </div>
+      </nav>
     </>
   );
 }
